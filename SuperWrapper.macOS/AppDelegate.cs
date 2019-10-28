61177731b0dc190436908a538ed3a97726dccd23
Gabriele Coppola <g.coppola.wp@gmail.com>
2019-10-28 18:22:18 +0100

    [START] move solution into git

---
+++ SuperWrapper.macOS/AppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using AppKit;
+using Foundation;
+
+namespace SuperWrapper.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : Xamarin.Forms.Platform.MacOS.FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+            this.MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false) {Title = "Mind of Ai"};
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override NSWindow MainWindow { get; }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ SuperWrapper/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SuperWrapper
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GmailButton_OnClicked(object sender, EventArgs e)
+        {
+            this.MainWebView.Source = "https://mail.google.com/";
+        }
+    }
+}
